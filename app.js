e3d46fc9ab4770f3d14284e96014bf701319263e
Ernesto Araiza <yo@ernestoaraiza.com>
2016-02-28 11:43:17 -0500

    allows cors Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ var Entrada = mongoose.model('Entrada', require('./entrada'));
 
 var app = express();
 
-app.get('/', function (req, res){
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
+app.get('/', function (req, res, next){
 	res.send("<a href='/noticias'>Mostrar noticias</a>");
 });
 
-app.get('/noticias', function (req, res){
+app.get('/noticias', function (req, res, next){
 	Entrada.find({}, function (err, docs) {
         res.json(docs);
     });
 });
 
 app.listen(3000, function(){
-	console.log('Noticias listening on port 3000');
+	console.log('Noticias listening on port 3000 - cors');
 });
